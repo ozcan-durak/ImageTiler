c558692ce2855b684f7a8b64769b01681bd0c9d9
ozcandurak <ozcan.durak@emlakjet.com>
2015-08-25 17:19:19 +0300

    Small fixes

--- app.js
+++ app.js
@@ -35,70 +35,123 @@ var url  = require('url');
 var im = require("imagemagick");
 var fs = require('fs');
 var mkdirp = require('mkdirp');
+//var nodemailer = require('nodemailer');
+var querystring = require('querystring');
+var favicon = require('serve-favicon');
+var finalhandler = require('finalhandler');
 
 // Here we start to create the sever
+
+var _favicon = favicon(__dirname + '/public/favicon.ico');
 var server = http.createServer(function(req, res) {
-console.time('execution_time');
-
-// Parsing String from the URL
-var url_parts = url.parse(req.url, true);
-var query = url_parts.query;
-var fileP = query.url;
-var fileUrl = String(query.url);
-
-var output = fileUrl.split('/');
-output.splice(6, 1);
-filePath = output.join('/');
-
-//explode the URL and gets the clean url to Origin of the image.
-var fileChecker = String(output[4]);
-
-var imageServe = function(){
-    fs.readFile(fileP + ".jpg", function (err, content) {
-          if (err) {
-              res.writeHead(400, {'Content-type':'text/html'});
-              res.end("Image cannot be served, Reason might be image not exist ! Please check the requested image on the server to verify");
-              console.log(err);
-          } else {
-              //specify the content type in the response will be an image
-              res.writeHead(200,{'Content-type':'image/jpg'});
-              res.end(content);
-          }
-      });  
 
 
-var convertFunct = function(filePath) {
-    if (!fs.existsSync(filePath)){
-         mkdirp(filePath);
-         console.log("File wasn't exists, so its been created = "+ fileChecker)
-         
-          var args = [
-          filePath+".jpg",                                                                              // image location
-          "-crop", "256x256",
-          "-strip", "-profile", "profile.icm",                                                          // will crop the tiles
-          "-quality", "90%",                                                                            // Tile Size
-          "-set",  "filename:tile",             
-          filePath+"/%[fx:page.x/256]-%[fx:page.y/256]", "%[filename:tile]-0.jpg"             // {512} value should be same as your tile size.                                       
-          ];
 
-          // Function to crop
-          im.convert(args, function(err) {
-            imageServe();
-             });
 
-          //Removes the undefined folder if created by Nodejs bug (related with :51)
-        if (fs.existsSync("undefined")) {
-          fs.rmdir("undefined");
-         };
-    } else {
-        imageServe();
-    }
-};
+    //to check execution time
+    console.time('execution_time');
 
-convertFunct(filePath);
+    // Parsing String from the URL
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var fileP = query.url;
+    var fileUrl = String(query.url);
+
+    var output = fileUrl.split('/');
+    output.splice(6, 1);
+    filePath = output.join('/');
+
+        //explode the URL and gets the clean url to Origin of the image.
+        var fileChecker = String(output[4]);
+
+        //send email if the service fails
+       /* var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'imagetiler@gmail.com',
+                pass: 'xxx'
+            }
+        });*/
+
+       /* var mailOptions = {
+            from: 'Image Tiler Service ✔ <imagetiler@gmail.com>', // sender address
+            to: 'ozcan.durak@ankageo.com', // list of receivers
+            subject: 'ImageTiler Has failed', // Subject line
+            text: 'Check immediately', // plaintext body
+            html: '<b>Image cannot be served, Reason might be image not exist ! Please check the requested image on the server to verify</b>' // html body
+        };*/
+
+        var imageServe = function() {
+            console.log("imageServe");
+            fs.readFile(fileP + ".jpg", function (err, content) {
+                if (err) {
+
+                    console.log("imageServe if ");
+                    res.writeHead(400, {'Content-type': 'text/html'});
+                    //res.end("Image cannot be served, Reason might be image not exist ! Please check the requested image on the server to verify");
+                    console.log(err);
+
+                } else {
+
+                    console.log("imageServe else ");
+                    //specify the content type in the response will be an image
+                    res.writeHead(200, {'Content-type': 'image/jpg'});
+                    res.end(content);
+                }
+            });
+        }
+
+        //Tiles the image.
+        var convertFunct = function(filePath) {
+
+            console.log("convertFunct ");
+
+            if (!fs.existsSync(filePath)){
+                console.log("convertFunct if ");
+
+
+                mkdirp(filePath);
+                 console.log("File wasn't exists, so its been created = "+filePath);
+
+                  var args = [
+                  filePath+".jpg",
+                  "-crop", "256x256",
+                  "-strip", "-profile", "profile.icm",
+                  "-quality", "90%",
+                  "-set",  "filename:tile",
+                  filePath+"/%[fx:page.x/256]-%[fx:page.y/256]", "%[filename:tile]-0.jpg"
+                  ];
+
+                  // Function to crop
+                  im.convert(args, function(err) {
+                    imageServe();
+                     });
+
+            } else {
+
+                console.log("convertFunct else ");
+
+                imageServe();
+            }
+        };
+
+
+    console.log("convertFunct cagirilacak ");
+
+        convertFunct(filePath);
+
+    var done = finalhandler(req, res);
+
+    _favicon(req, res, function onNext(err) {
+
+
+        res.statusCode = 404;
+    });
+
+    console.log("convertFunct cagirilacak bitti");
 
     //Check the execution time
     console.timeEnd('execution_time');
 }).listen(8080, function(){
-  console.log('http://localhost:8080?url=IMAGE_URL_HERE');
-})
+  console.log('http://localhost:8080?url=IMAGE_URL_HERE_WITHOUT')
+});
